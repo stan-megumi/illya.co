e0ee16341fabfea0295db129466992bd3e9c7051
stan-megumi <jinx@openmailbox.org>
2018-06-14 20:37:47 +0800

    init

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((projectile-project-compilation-cmd . "npm start")
+         ( projectile-project-run-cmd . "npm start")
+         ( projectile-project-test-cmd . "cd production && qmake ../kato.pro")
+)))
