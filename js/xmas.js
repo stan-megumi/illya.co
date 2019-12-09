f8f9db981e298ec2cbb9d5af7fef25659ccfeed9
stan-megumi <jinx@openmailbox.org>
2019-12-10 01:12:59 +0800

    automatic push

--- js/xmas.js
+++ js/xmas.js
@@ -132,8 +132,18 @@ const app = new Vue({
 	var ctx_right = canvas_right.getContext('2d');
 	var canvas_center  = document.getElementById("center")
 	var ctx_center = canvas_center.getContext('2d');
+	var canvas_left  = document.getElementById("left")
+	var ctx_left = canvas_left.getContext('2d');
+	var canvas  = document.getElementById("origin")
+	canvas.style.display = "none"
+
 	
-	var canvas  = document.getElementById("left")
+	var dick_src = "img/dick.png"    
+	var left_src = "img/left.jpg"
+	var right_src = "img/right.jpg"
+	var center_src = "img/center.jpg"
+
+	
 	if (canvas.getContext) {   
 	    var ctx = canvas.getContext('2d');
 	    var imageObj = new Image();
@@ -144,10 +154,29 @@ const app = new Vue({
 	}
 
 	var imageObj1 = new Image();
-	imageObj1.src = "https://5b0988e595225.cdn.sohucs.com/images/20171224/e67e460b96f343c79b10af2c277bd23f.jpeg"
+	imageObj1.src = dick_src
 	imageObj1.onload = function () {
 	    loaded_dick = 1;
 	}
+
+	var imageObj2 = new Image();
+	imageObj2.src = left_src
+	imageObj2.onload = function () {
+	    loaded_left = 1;
+	}
+
+	var imageObj3 = new Image();
+	imageObj3.src = center_src
+	imageObj3.onload = function () {
+	    loaded_center = 1;
+	}
+
+	var imageObj4 = new Image();
+	imageObj4.src = right_src
+	imageObj4.onload = function () {
+	    loaded_right = 1;
+	}
+	
 	
 	var timer = setInterval( function () {
 	    if (loaded_origin == 1) {
@@ -159,18 +188,33 @@ const app = new Vue({
 	    }
 	    
 	    if (loaded_dick == 1 && loaded_origin == 2) {
-
-		ctx_dick.rotate(Math.PI * 2 / 360 * -45);
-		ctx_dick.drawImage (imageObj1,-150,20,150,150)
-		ctx_dick.rotate(Math.PI * 2 / 360 * 45);
-
+		ctx_dick.drawImage (imageObj1,0,20,320,320)
 		ctx_dick.font = '700 30px Raleway';
 		ctx_dick.fillStyle = 'white';
 		ctx_dick.fillText(msg,(320 - msg.length * 30)/2,300);
 		counter += 1;
 	    }
 
-	    if (counter == 1) {
+	    if (loaded_left == 1 && loaded_origin == 2) {
+		ctx_left.drawImage (imageObj2,2,20,320,320)
+		ctx_left.font = '700 30px Raleway';
+		counter += 1;
+	    }
+
+	    if (loaded_center == 1 && loaded_origin == 2) {
+		ctx_center.drawImage (imageObj3,0,20,320,320)
+		ctx_center.font = '700 30px Raleway';
+		counter += 1;
+	    }
+
+	    if (loaded_right == 1 && loaded_origin == 2) {
+		ctx_right.drawImage (imageObj4,0,20,320,320)
+		ctx_right.font = '700 30px Raleway';
+		counter += 1;
+	    }
+	    
+
+	    if (counter == 4) {
 		clearInterval(timer);
 	    }
 	}, 10);
